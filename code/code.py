918ec46922993d6388ea12d38a97ade15d0c4cd4
Fadi Sedik <fady88@gmail.com>
2020-12-25 11:04:29 +0100

    adding initial code

---
+++ code/code.py
@@ -0,0 +1,12 @@
+import time,os
+
+def profiler(method):
+    def wrapper_method(*arg, **kw):
+        t = time.time()
+        ret = method(*arg, **kw)
+        print('Method '  + method.__name__ +' took : ' + "{:2.5f}".format(time.time()-t) + ' sec')
+        return ret
+    return wrapper_method
+
+if __name__ == "__main__":
+    pass
