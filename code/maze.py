34cfb773c23a1efdaee5125334ca8ab7bc0b45c8
Fadi Sedik <fady88@gmail.com>
2021-01-03 11:05:58 +0100

    adding maze solver and output

--- code/maze.py
+++ code/maze.py
@@ -1,4 +1,6 @@
-import time
+import time,re
+from collections import defaultdict
+from itertools import permutations  
 
 
 def profiler(method):
@@ -12,14 +14,16 @@ def profiler(method):
 
 
 class vm:
-    @profiler
     def __init__(self, _file):
         self.pc = 0
         self.stack = []
         self.txt = []
         self.terminated = False
+        self.requesting_input = False
         self.reg = [0] * 8
         self.input = []
+        self.output = []
+
         with open(_file, 'rb') as f:
             b = f.read(2)
             while b != b'':
@@ -29,9 +33,19 @@ class vm:
         self.ops_map = \
             {0: self.halt_0, 1: self.set_1, 2: self.push_2, 3: self.pop_3, 4: self.eq_4,
              5: self.gt_5, 6: self.jmp_6, 7: self.jt_7, 8: self.jf_8, 9: self.add_9,
-             10: self.mul_10, 11: self.mod_11, 12: self.and_12, 13: self.or_13, 14: self.not_14,
-             15: self.rmem_15, 16: self.wmem_16, 17: self.call_17,
+             10: self.mul_10, 11: self.mod_11, 12: self.and_12, 13: self.or_13,
+             14: self.not_14, 15: self.rmem_15, 16: self.wmem_16, 17: self.call_17,
              18: self.ret_18, 19: self.out_19, 20: self.in_20, 21: self.noop_21}
+
+    def set_input(self, inp):
+        self.input = list(inp)
+        self.input.append('\n')
+        self.requesting_input = False
+
+    def get_output(self):
+        tmp = self.output
+        self.output = []
+        return tmp
 
     def get_value_by_offest(self, offset):
         param = self.txt[self.pc + offset]
@@ -162,15 +176,15 @@ class vm:
             self.pc = self.stack.pop()
 
     def out_19(self):
-        print(chr(self.get_value_by_offest(1)), end='')
+        self.output.append(chr(self.get_value_by_offest(1)))
         self.pc += 2
 
     def in_20(self):
         if len(self.input) == 0:
-            self.input = list(input())
-            self.input.append('\n')
-        self.set_value_by_offset(1, ord(self.input.pop(0)))
-        self.pc += 2
+            self.requesting_input = True
+        else:
+            self.set_value_by_offset(1, ord(self.input.pop(0)))
+            self.pc += 2
 
     def noop_21(self):
         self.pc += 1
@@ -179,12 +193,104 @@ class vm:
         assert self.pc < len(self.txt)
         self.ops_map[self.txt[self.pc]]()
 
-    @profiler
     def run(self):
-        print()
-        while not self.terminated:
+        while not self.terminated and not self.requesting_input:
             self.cycle()
 
+
 if __name__ == "__main__":
-    obj = vm('input/challenge.bin')
-    obj.run()
+
+    lantern_path = ['doorway', 'north', 'north',
+                    'bridge', 'continue', 'down', 'east']
+    can_path = ['west', 'west', 'passage', 'ladder', 'west', 'south', 'north']
+    to_visit = [[]]
+    
+    map = {}
+    infos = {}
+    visited = defaultdict(int)
+    values = {}
+
+    while to_visit:
+        current_path = to_visit.pop(0)
+        # print(current_path)
+
+        obj = vm('input/challenge.bin')
+        obj.run()
+
+        for step in lantern_path:
+            obj.set_input(step)
+            obj.output = []
+            obj.run()
+
+        obj.set_input('take empty lantern'), obj.run()
+        obj.output = []
+
+        for step in can_path:
+            obj.set_input(step)
+            obj.output = []
+            obj.run()
+
+        obj.set_input('take can'), obj.run()
+        obj.set_input('use can'), obj.run()
+        obj.set_input('use lantern'), obj.run()
+
+        for step in current_path:
+            obj.set_input(step)
+            obj.output = []
+            obj.run()
+
+        tmp = ''.join(obj.get_output())
+
+        visited[tmp] += 1
+
+        if obj.terminated or visited[tmp] > 3:
+            continue
+
+        nxt = re.findall(r'(?:- (\w+ ?\w+)\n)', tmp)
+
+        objs = [ele for ele in nxt if tmp.find('exit') > tmp.find('- ' + ele)]
+        steps = [ele for ele in nxt if tmp.find('exit') < tmp.find('- ' + ele)]
+
+        for ele in steps:
+            new_path = current_path.copy()
+            new_path.append(ele)
+            to_visit.append(new_path)
+
+        for ele in objs:
+            map[ele] = current_path
+            print(ele, current_path)
+            obj.set_input('look ' + ele), obj.run()
+            infos[ele] = obj.get_output()
+
+            if 'pentagon' in ''.join(infos[ele]):
+                values[5] = ele
+            elif 'triangle' in ''.join(infos[ele]):
+                values[3] = ele
+            elif 'nine' in ''.join(infos[ele]):
+                values[9] = ele
+            elif 'seven' in ''.join(infos[ele]):
+                values[7] = ele
+            elif 'two' in ''.join(infos[ele]):
+                values[2] = ele
+
+    vals = values.keys()
+    order = ''
+    for perm in permutations(vals):
+        if perm[0] + perm[1] * perm[2]**2 + perm[3]**3 - perm[4] == 399:
+            for i in perm :
+                order += values[i] +', '
+            break
+
+
+    with open('output.txt', 'w+') as f:
+        for ele in visited:
+            f.write(ele + '\n')
+            f.write('--------------------------------------\n')
+
+        f.write('***************************************\n\n\n')
+        for ele in map:
+            f.write(ele + ' -> ')
+            f.write(str(map[ele]) + '\n')
+            f.write(''.join(infos[ele]) + '\n\n')
+
+        f.write(order)
